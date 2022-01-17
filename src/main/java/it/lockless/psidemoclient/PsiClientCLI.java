ae0333bee72e41ce637725838d53d061ace42aa3
Stefano Conoci <conoci@lockless.it>
2022-01-17 15:31:42 +0100

    Initialized maven project with picocli

---
+++ src/main/java/it/lockless/psidemoclient/PsiClientCLI.java
@@ -0,0 +1,16 @@
+package it.lockless.psidemoclient;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "psiClientCLI", mixinStandardHelpOptions = true, version = "psiClientCLI 1.0", description = "Demo implementation of a PSI client using the psi-sdk.")
+public class PsiClientCLI implements Runnable{
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new it.lockless.psidemoclient.PsiClientCLI()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello world");
+    }
+}
