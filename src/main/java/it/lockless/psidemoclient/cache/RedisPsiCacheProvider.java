b91f233cf9ae3fc5bd45c667e9d46bc0eaacaa4d
Stefano Conoci <conoci@lockless.it>
2022-01-21 14:38:14 +0100

    Modified redis cache provider to support multithreading

--- src/main/java/it/lockless/psidemoclient/cache/RedisPsiCacheProvider.java
+++ src/main/java/it/lockless/psidemoclient/cache/RedisPsiCacheProvider.java
@@ -2,30 +2,56 @@ package it.lockless.psidemoclient.cache;
 
 import psi.cache.PsiCacheProvider;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.Optional;
 
+// Helpful commands to run redis on docker
+//
 // Create redis docker: docker run --name redis -p 6379:6379 -d redis
+// After stopped, can run again with: docker start redis
 // Connect to cli: docker exec -it redis redis-cli
+
 public class RedisPsiCacheProvider implements PsiCacheProvider {
 
-    private final Jedis jedis;
+    //private final Jedis jedis;
+    private final JedisPool jedisPool;
 
     public RedisPsiCacheProvider(String host, int port) {
-        this.jedis = new Jedis(host,port);
+        this.jedisPool = new JedisPool(host, port);
+        this.jedisPool.getResource().ping();
     }
-
+    /**
+     * Retrieve the value linked to a given key.
+     *
+     * @param key   key corresponding to the value to be retrieved
+     *
+     * @return an Optional containing the cached value if present, Optional.empty() otherwise
+     */
     @Override
-    public Optional<String> get(String s) {
-        String cachedResponse = jedis.get(s);
+    public Optional<String> get(String key) {
+        String cachedResponse;
+        Jedis jedis = this.jedisPool.getResource();
+        cachedResponse = jedis.get(key);
+        this.jedisPool.returnResource(jedis);
         if(cachedResponse == null)
             return Optional.empty();
         else return Optional.of(cachedResponse);
     }
 
+    /**
+     * Stores the pair <key, value> into the cache. If the key exists, it is not replaced
+     *
+     * @param key       key corresponding to the value to be stored.
+     * @param value     value to be stored.
+     */
     @Override
-    public void put(String s, String s1) {
-        if(jedis.setnx(s, s1) == 0)
+    public void put(String key, String value) {
+        Jedis jedis = this.jedisPool.getResource();
+        long response = jedisPool.getResource().setnx(key, value);
+        this.jedisPool.returnResource(jedis);
+        if (response == 0)
             throw new RedisKeyAlreadyWrittenException();
     }
+
 }
